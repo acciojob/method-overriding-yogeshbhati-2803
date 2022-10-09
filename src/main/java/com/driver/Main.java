4fca4d0504b3ced857fd66c98c93792b239e54e7
Yogesh Bhati <yogesh.bhati@indiamart.com>
2022-10-09 18:42:50 +0530

    add class in main

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,24 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+        B b=new B();
+
+        System.out.println(b.meth());
+
+    }
+    public static class A {
+        public String meth()
+        {
+            return "Invoking method from class A";
+        }
+    }
+
+    public static class B extends A{
+        public String meth()
+        {
+            return "Method is overridden in Extendend class B";
+        }
+    }
+
 }
